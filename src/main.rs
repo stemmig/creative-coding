35c82153792424d56fc568d9b61d70bff0c3dc87
apstemmer <apstemmer@hotmail.com>
2023-12-11 15:18:47 -0800

    creates blank window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+mod window;
+
+#[cfg(target_arch="wasm32")]
+use wasm_bindgen::prelude::*;
+
+#[cfg_attr(target_arch="wasm32", wasm_bindgen(start))]
 fn main() {
+    window::run();
     println!("Hello, world!");
 }
